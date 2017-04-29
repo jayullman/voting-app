6b0e75034f61157415177f43f73fcba281eeaf7a
Jay Ullman <jay@Jays-MacBook-Air.local>
2017-04-29 16:12:22 -0700

    Update mongoDB url

--- controllers/connectToDb.js
+++ controllers/connectToDb.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
-url = 'mongodb://localhost:27017/voting';
+const url = process.env.MONGOLAB_URI;
 const db = mongoose.connection;
+const options = {
+  server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+  replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }
+};
 
 module.exports = function() {
-  mongoose.connect(url);
+  mongoose.connect(url, options);
   db.on('open', () => {
     console.log('Connected to database');
   });
